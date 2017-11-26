1d7d55e8b9b7bc03002226e3df089b9eab447100
Jan Bodnar <jan.bodnar@gmail.com>
2017-11-26 14:35:29 +0100

    Create ListFilesEx.java

---
+++ ListFilesEx.java
@@ -0,0 +1,18 @@
+package com.zetcode;
+
+import java.io.File;
+import java.util.Collection;
+import org.apache.commons.io.FileUtils;
+
+public class ListFilesEx {
+    
+    public static void main(String[] args) {
+        
+        File myDir = new File("/home/janbodnar/tmp");
+        
+        Collection<File> files = FileUtils.listFiles(myDir, 
+                new String[] {"txt", "html"}, true);
+        
+        files.stream().forEach(System.out::println);
+    }
+}
